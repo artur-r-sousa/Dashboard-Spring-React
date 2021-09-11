7e809e7a139588b85a6a24afded9778f882e7f85
Artur Sousa <artursousa505@gmail.com>
2021-09-11 17:22:09 -0300

    Pagination

---
+++ backend/src/main/java/com/artur/dsvendas/service/SaleService.java
@@ -0,0 +1,29 @@
+package com.artur.dsvendas.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.artur.dsvendas.dto.SaleDTO;
+import com.artur.dsvendas.entities.Sale;
+import com.artur.dsvendas.repositories.SaleRepository;
+import com.artur.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+}
